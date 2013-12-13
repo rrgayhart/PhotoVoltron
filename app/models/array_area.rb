5749abbc9ff8e624622909b24762bb0f0f27c5e3
Meeka <rrgayhart@gmail.com>
2013-12-13 14:45:18 -0700

    Sets up rspec testing

--- app/models/array_area.rb
+++ app/models/array_area.rb
@@ -1,15 +1,30 @@
 class ArrayArea
+  require 'mathn'
 
-  def desired_peak_array_power(kwdc)
-    kwdc
+  def initialize(p,n)
+    @desired_peak_array_power = p
+    @module_efficiency = to_fraction(n)
   end
 
-  def module_efficiency(percent)
-    percent
+  def display_array_area
+    area = display_singular(estimated_array_area)
+    return "#{area}m squared"
   end
 
-  def estimated_array_area(p,n)
-    desired_peak_array_power(p) / ((kw/meter_squared(1)) * module_efficiency(n))
+  def estimated_array_area
+    @desired_peak_array_power / ((kw/meter_squared(1)) * @module_efficiency)
+  end
+
+  def to_fraction(percent)
+    percent/100
+  end
+
+  def display_singular(fraction)
+    fraction.ceil
+  end
+
+  def display_two_decimals(fraction)
+    fraction.ceil(2).to_f
   end
 
   def kw
---
+++ spec/models/array_area_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ArrayArea do
+  it "returns a string of the array area with display_array_area" do
+    array = ArrayArea.new(5,12)
+    expect(array.display_array_area).to eq("42m squared")
+  end
+
+  it "returns a fraction with estimated_array_area" do
+    array = ArrayArea.new(5,12)
+    expect(array.estimated_array_area).to eq((125/3))
+  end
+
+  it "calculates the fraction for a percent with to_fraction" do
+    array = ArrayArea.new(5,12)
+    fraction = array.to_fraction(12)
+    expect(fraction).to eq((3/25))
+    expect(fraction.to_f).to eq(0.12)
+  end
+end
